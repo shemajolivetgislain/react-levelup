cc5d86d23d8d7ca8f00015b756a718c61a171799
shema jolivet gislain <shemajolivet@gmail.com>
2023-05-28 01:35:29 +0200

    useState recap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import "./App.css";
-import React from "react";
+import { React, useState } from "react";
 import Tooglestatus from "./components/Tooglestatus";
 import Login from "./Pages/Login";
 import Home from "./Pages/Home";
+import Input from "./components/Input";
 
 function App() {
+  const [hide, setHide] = useState(false);
+
+  const hideComponent = () => setHide((previous) => !previous);
   return (
     <div>
-
-      <Tooglestatus title="Reusable component" content= "here is how make reusable component"/>
-      <Login/>
-      <Home/>
-
+      <Tooglestatus
+        title="Reusable component"
+        content="here is how make reusable component"
+      />
+      <Login />
+      <button onClick={hideComponent}>
+        {/* this if hide state is true then show else Hide */}
+        {hide ? "Show" : "Hide"}
+      </button>
+      {/* <Input type="submit" value="Hide" onClick = {hideComponent}/> */}
+      {hide && <Home />}
     </div>
   );
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-function Input({type, value, onChange,name}) {
-  return <input type={type} onChange={onChange} name={name} value={value}/>;
+function Input({type, value, onChange,name, onClick}) {
+  return <input type={type} onChange={onChange} onClick={onClick} name={name} value={value}/>;
 }
 
 export default Input
